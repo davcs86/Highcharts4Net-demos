b5799810f936a9da0735080ffd7171234518e237
davcs86 <davcs86@gmail.com>
2015-11-19 18:22:35 -0600

    New demos

--- Highcharts4Net-demos/Controllers/AreaChartsController.cs
+++ Highcharts4Net-demos/Controllers/AreaChartsController.cs
@@ -7,55 +7,46 @@ namespace Highcharts4Net_demos.Controllers
 
         public ActionResult Basic()
         {
-            
             return View();
         }
 
         public ActionResult Negative()
         {
-
             return View();
         }
 
         public ActionResult Stacked()
         {
-
             return View();
         }
 
         public ActionResult Percentage()
         {
-
             return View();
         }
 
         public ActionResult MissingPoints()
         {
-
             return View();
         }
 
         public ActionResult InvertedAxes()
         {
-
             return View();
         }
 
         public ActionResult Areaspline()
         {
-
             return View();
         }
 
         public ActionResult Arearange()
         {
-
             return View();
         }
 
         public ActionResult ArearangeWithLine()
         {
-
             return View();
         }
     }
---
+++ Highcharts4Net-demos/Controllers/ColumnAndBarChartsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Highcharts4Net_demos.Controllers
+{
+    public class ColumnAndBarChartsController : Controller
+    {
+        // GET: ColumnAndBarCharts
+        public ActionResult Index()
+        {
+            return RedirectToAction("BasicBar");
+        }
+
+        public ActionResult BasicBar()
+        {
+            return View();
+        }
+
+        public ActionResult StackedBar()
+        {
+            return View();
+        }
+
+        public ActionResult BarWithNegatives()
+        {
+            return View();
+        }
+
+        public ActionResult BasicColumn()
+        {
+            return View();
+        }
+
+        public ActionResult ColumnWithNegatives()
+        {
+            return View();
+        }
+
+        public ActionResult StackedColumn()
+        {
+            return View();
+        }
+    }
+}
